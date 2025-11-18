a303ec6b7f67550864db5dc448022d59628f4fa0
Mukul3432 <dirtytaj3@gmail.com>
2025-11-18 21:04:41 +0530

    staged changes testing

--- print_get_compare.c
+++ print_get_compare.c
@@ -1,27 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    char buffer_printf[100];
-    char buffer_gets[100];
-
+    char b[50];
+    char a[50];
     printf("--- Comparing printf and gets ---\n\n");
-
-    // Using printf to display a strin
     printf("Enter a string for scanf (max 99 chars): ");
-    // Using scanf to read a string
-    scanf("%s", buffer_printf);
-    printf("printf output: %s\n\n", buffer_printf);
-
-    // Using gets to read a string
-    printf("Enter a string for gets (BE CAREFUL, NO BOUNDS CHECKING!): ");
-    gets(buffer_gets); // WARNING: gets is unsafe and should not be used in new code.
-                       // It does not check buffer boundaries, leading to buffer overflows.
-    printf("gets output: %s\n\n", buffer_gets);
-
-    printf("--- Differences ---\n");
-    printf("printf: Used for formatted output to the console. It's safe when used correctly with format specifiers.\n");
-    printf("gets: Used for reading a line of text from standard input. It is UNSAFE because it doesn't prevent buffer overflows.\n");
-    printf("Recommendation: Always use fgets instead of gets for input, as fgets allows specifying the buffer size.\n");
-
+    scanf("%s", a);
+    printf("scanf output: %s\n", a);
+    // gets(b);
+    // printf("gets output: %s", b);
+    // Note: 'gets' is unsafe and has been removed from the C11 standard onwards.
     return 0;
 }
